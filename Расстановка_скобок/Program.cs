336a58bf5a2b627f3bce71bcb863f45cf8d926f9
stald10 <70633097+stald10@users.noreply.github.com>
2026-06-20 15:28:02 +0700

    Добавьте файлы проекта.

---
+++ Расстановка_скобок/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+namespace BracketsSystem
+
+{
+    public static class Brackets
+    {
+        private static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+            int count_brack = check_brackets(str);
+            if (count_brack == 0)
+            {
+                Console.WriteLine("Success");
+            }
+            else
+            {
+                Console.WriteLine(count_brack);
+            }
+        }
+
+
+        public static int check_brackets(string str)
+        {
+            Stack<(char, int)> brackets = new();
+
+            for (int i = 0; i < str.Length; ++i)
+            {
+                char c = str[i];
+                if (c == '(' || c == '[' || c == '{') brackets.Push((c, i + 1));
+                else if (c == ')' || c == ']' || c == '}')
+                {
+                    (char, int) head;
+                    var res = brackets.TryPop(out head);
+                    if (!res) return i+1;
+                    if (
+                        (head.Item1 == '[' && c != ']') ||
+                        (head.Item1 == '(' && c != ')') ||
+                        (head.Item1 == '{' && c != '}')
+                        ) return i+1;
+                }
+            }
+
+            foreach (var elem in brackets)
+            {
+                var item = brackets.Pop();
+                return item.Item2;
+ 
+            }
+            return 0;
+
+        }
+    }
+
+}
---
+++ Тесты_Расстановка_Скобок/Tests.cs
@@ -0,0 +1,49 @@
+﻿using BracketsSystem;
+
+public class Tests
+{
+    [Fact]
+    public void TestOpening()
+    {
+        Assert.Equal(1, Brackets.check_brackets("{"));
+        Assert.Equal(1, Brackets.check_brackets("{[]"));
+        Assert.Equal(3, Brackets.check_brackets("{{{"));
+        Assert.Equal(3, Brackets.check_brackets("[]([]"));
+        Assert.Equal(3, Brackets.check_brackets("{{{[][][]"));
+        Assert.Equal(6, Brackets.check_brackets("{{{{{{{((()))}"));
+        Assert.Equal(5, Brackets.check_brackets("{()}{"));
+    }
+    [Fact]
+    public void TestClosing()
+    {
+        Assert.Equal(1, Brackets.check_brackets("}()"));
+        Assert.Equal(3, Brackets.check_brackets("()}()"));
+        Assert.Equal(1, Brackets.check_brackets("}()"));
+        Assert.Equal(7, Brackets.check_brackets("{[()]}}()"));
+        Assert.Equal(13, Brackets.check_brackets("dasdsadsadas]]]"));
+    }
+    [Fact]
+    public void TestSuccess()
+    {
+        Assert.Equal(0, Brackets.check_brackets("{}()"));
+        Assert.Equal(0, Brackets.check_brackets("({}[(((())))])"));
+        Assert.Equal(0, Brackets.check_brackets("()"));
+        Assert.Equal(0, Brackets.check_brackets("({})"));
+        Assert.Equal(0, Brackets.check_brackets("foo(bar[i]);"));
+    }
+    [Fact]
+    public void TestPerson() 
+    {
+        Assert.Equal(0, Brackets.check_brackets("([](){([])})"));
+        Assert.Equal(5, Brackets.check_brackets("()[]}"));
+        Assert.Equal(7, Brackets.check_brackets("{{[()]]"));
+        Assert.Equal(3, Brackets.check_brackets("{{{[][][]"));
+        Assert.Equal(3, Brackets.check_brackets("{*{{}"));
+        Assert.Equal(2, Brackets.check_brackets("[[*"));
+        Assert.Equal(2, Brackets.check_brackets("{{"));
+        Assert.Equal(3, Brackets.check_brackets("{{{**[][][]"));
+
+    }
+
+}
+
