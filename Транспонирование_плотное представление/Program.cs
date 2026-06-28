af417f5f5e9cc569d39c94c6d5cb41b12020e73d
stald10 <70633097+stald10@users.noreply.github.com>
2026-06-28 21:16:29 +0700

    Транспонирование в плотном формате

---
+++ Транспонирование_плотное представление/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var matrixSize = Console.ReadLine()!.Split(" ");
+
+        int N = int.Parse(matrixSize[0]);
+        int M = int.Parse(matrixSize[1]);
+
+        List<List<int>> matrix = new();
+        List<List<int>> matrixTranpose = new();
+
+        for (int i = 0; i < M; ++i) 
+        {
+            matrixTranpose.Add(Enumerable.Repeat(0, N).ToList());
+        }
+
+        for (int i = 0; i < N; ++i) 
+        {
+            matrix.Add(Console.ReadLine()!.Split(" ").Select(x => int.Parse(x)).ToList());
+        }
+
+        for (int i = 0; i < M; ++i)
+        {
+            for (int j = 0; j < N; ++j) 
+            {
+                matrixTranpose[i][j] = matrix[N- j - 1][i];
+            }
+        }
+
+        Console.WriteLine($"{M} {N}");
+        foreach (var elem in matrixTranpose) 
+        {
+            Console.WriteLine(string.Join(" ", elem));
+        }
+
+    }
+}
